26dd9b6b7bf8d45078e37b724824a9329d131a8b
33-Maryam <maryamnoorety33@gmail.com>
2023-02-19 21:53:40 +0600

    event handeling javascript function added

---
+++ js/script.js
@@ -0,0 +1,95 @@
+// event handling area
+
+document.getElementById("btn-blog").addEventListener("click", function () {
+    window.location.href = "answers.html";
+  });
+  
+  document
+    .getElementById("calculate-triangle")
+    .addEventListener("click", function () {
+      const triangleB = getInputField("triangle-b");
+      const triangleH = getInputField("triangle-h");
+      const triangleTitle = getTextElement("triangle");
+      const isValid = inputValidation(triangleB, triangleH);
+      if (!isValid) {
+        return;
+      }
+      const triangleArea = triangleRhombusPentagonArea(triangleB, triangleH);
+      setElement(triangleTitle, triangleArea);
+    });
+  
+  document
+    .getElementById("calculate-rectangle")
+    .addEventListener("click", function () {
+      const rectangleW = getInputField("rectangle-w");
+      const rectangleL = getInputField("rectangle-l");
+      const rectangleTitle = getTextElement("rectangle");
+      const isValid = inputValidation(rectangleW, rectangleL);
+      if (!isValid) {
+        return;
+      }
+      const rectangleArea = rectangleParallelogramArea(rectangleW, rectangleL);
+      setElement(rectangleTitle, rectangleArea);
+    });
+  
+  document
+    .getElementById("calculate-parallelogram")
+    .addEventListener("click", function () {
+      const parallelogramB = getInputField("parallelogram-b");
+      const parallelogramL = getInputField("parallelogram-h");
+      const parallelogramTitle = getTextElement("parallelogram");
+      const isValid = inputValidation(parallelogramB, parallelogramL);
+      if (!isValid) {
+        return;
+      }
+  
+      const parallelogramArea = rectangleParallelogramArea(
+        parallelogramB,
+        parallelogramL
+      );
+  
+      setElement(parallelogramTitle, parallelogramArea);
+    });
+  
+  document
+    .getElementById("calculate-rhombus")
+    .addEventListener("click", function () {
+      const rhombusD1 = getInputField("rhombus-d1");
+      const rhombusD2 = getInputField("rhombus-d2");
+      const rhombusTitle = getTextElement("rhombus");
+      const isValid = inputValidation(rhombusD1, rhombusD2);
+      if (!isValid) {
+        return;
+      }
+      const rhombusArea = triangleRhombusPentagonArea(rhombusD1, rhombusD2);
+      setElement(rhombusTitle, rhombusArea);
+    });
+  
+  document
+    .getElementById("calculate-pentagon")
+    .addEventListener("click", function () {
+      const pentagonP = getInputField("pentagon-p");
+      const pentagonB = getInputField("pentagon-b");
+      const pentagonTitle = getTextElement("pentagon");
+      const isValid = inputValidation(pentagonP, pentagonB);
+      if (!isValid) {
+        return;
+      }
+      const pentagonArea = triangleRhombusPentagonArea(pentagonP, pentagonB);
+      setElement(pentagonTitle, pentagonArea);
+    });
+  
+  document
+    .getElementById("calculate-ellipse")
+    .addEventListener("click", function () {
+      const ellipseA = getInputField("ellipse-a");
+      const ellipseB = getInputField("ellipse-b");
+      const ellipseTitle = getTextElement("ellipse");
+      const isValid = inputValidation(ellipseA, ellipseB);
+      if (!isValid) {
+        return;
+      }
+      const ellipseArea = triangleRhombusPentagonArea(ellipseA, ellipseB);
+      setElement(ellipseTitle, ellipseArea);
+    });
+  
